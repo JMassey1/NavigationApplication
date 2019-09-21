71737dcbc36aafcf58194a665268bb037934fd80
JMassey1 <j.massey1@bssdstd.net>
2019-09-20 23:50:52 -0500

    Created new assets folder for the csv files, felt more organized this way. Also redid the csvRead function to try and make it more efficient, but I can't test it on this computer, so we'll have to see if it worked monday.

--- app/src/main/java/com/jagnav/Map.java
+++ app/src/main/java/com/jagnav/Map.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 public class Map {
 
+    List<String[]> schoolMap;
+
     public Map() {
 
     }
@@ -15,6 +17,8 @@ public class Map {
         System.out.println("Kylie is dumb");
     }
 
+
+    /*
     public List<String[]> csvRead() throws IOException{
         List<String[]> list = new ArrayList<>();
         Reader reader1 = new FileReader("file.csv");
@@ -27,5 +31,19 @@ public class Map {
         csvRead.close();
         return list;
 
+    }
+    */
+
+    public void csvRead() throws IOException {
+        CSVReader reader = new CSVReader(new FileReader("JagNavMap(INCOMPLETE_FOR_TESTING).csv"));
+        schoolMap = reader.readAll();
+    }
+
+    public void printMap() {
+        for (String[] x: schoolMap){
+            for (String y: x){
+                System.out.println(y);
+            }
+        }
     }
 }
