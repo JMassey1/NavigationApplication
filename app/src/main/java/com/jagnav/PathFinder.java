a321bd45fe258aa80cf6462cefcdbc05b97f28c0
Henry Cooper <h.cooper@bssdstd.net>
2019-11-25 08:59:17 -0600

    renamed the PathFinder class to accomodate industry standardsn and fixed the return of its method so it actually returns a string array. (Most of today was brainstorming)

---
+++ app/src/main/java/com/jagnav/PathFinder.java
@@ -0,0 +1,13 @@
+package com.jagnav;
+import java.util.ArrayList;
+import com.jagnav.Location;
+
+public abstract class PathFinder {
+
+    public static String[] findPath(ArrayList<ArrayList<Location>> y) {
+        String[] x = {"test", "test", "test"};
+        return x;
+    }
+
+
+}
--- app/src/main/java/com/jagnav/pathFinder.java
+++
@@ -1,9 +0,0 @@
-package com.jagnav;
-
-public abstract class pathFinder {
-
-    public static String[] findPath(ArrayList<ArrayList<Location>>) {
-        return String[]
-    }
-
-}
