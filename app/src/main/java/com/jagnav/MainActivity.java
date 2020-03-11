7dadb81eb3eba2ac6244ce2548a96cf99e29042d
Jordan Massey <j.massey1@bssdstd.net>
2020-03-11 12:43:08 -0500

    started testing various front-end data collections, such as text input for room nums as well as a popup that occurs with route information once a route has been established

--- app/src/main/java/com/jagnav/MainActivity.java
+++ app/src/main/java/com/jagnav/MainActivity.java
@@ -1,29 +1,26 @@
 package com.jagnav;
 
+import android.app.Dialog;
 import android.content.res.AssetManager;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.navigation.NavigationView;
-import com.google.android.material.snackbar.Snackbar;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     String routeStart, routeEnd;
     EditText startInput, endInput;
-    Button submitButton;
+    TextView route;
+    Button submitButton, popUp, routeClose;
+    Dialog dia;
+
+    ArrayList<Location> path;
 
 
     @Override
@@ -31,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        dia = new Dialog(this);
         startInput = findViewById(R.id.startingLoc);
         endInput = findViewById(R.id.endLoc);
         submitButton = findViewById(R.id.button);
@@ -50,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
                 PathFinder pf = new PathFinder(test);
                 try {
                     //inputs should be in terms of room number or the equivalent cell in the csv x
-                    pf.findPath(test.findLocation(routeStart), test.findLocation(routeEnd));
+                    path = pf.findPath(test.findLocation(routeStart), test.findLocation(routeEnd));
 
                 }
                 catch (Exception l) {
@@ -59,12 +57,33 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        popUp = findViewById(R.id.popUp);
+        popUp.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View v) {
+                if (path != null && path.size() > 0) {
+                    showPopup(v, path);
+                }
+            }
 
+        });
+    }
 
+    public void showPopup(View v, ArrayList<Location> arr) {
 
+        route = findViewById(R.id.routeText);
+        routeClose = findViewById(R.id.routeCloseButton);
+        dia.setContentView(R.layout.route_popup);
+        routeClose.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View v) {
+                dia.dismiss();
+            }
 
+        });
+        dia.show();
     }
 
 }
